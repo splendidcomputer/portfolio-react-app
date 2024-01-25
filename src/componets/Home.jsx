870d233d342701c2ee65c01f5ff1fb1fb4c23822
Mostafa Razavi <mostafa@AORUS.mostafa-razavi.com>
2024-01-25 10:07:58 +0100

    Modified my description.

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
           Mostafa Razavi
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer
+          I'm an AI-inspired Web Developer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I'm a Full Stack Developer speiclazied in React
