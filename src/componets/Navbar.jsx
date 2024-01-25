0e064e72e67cb142d5dcb0bfcdabb12549d478dc
Mostafa Razavi <mostafa@AORUS.mostafa-razavi.com>
2024-01-25 12:04:47 +0100

    Modified the logo.

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import Logo from "../assets/photo.jpeg";
+import Logo from "../assets/logo2.png";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-      <div className="rounded-full overflow-hidden mt-48">
+      <div className="rounded-full overflow-hidden mt-auto">
         <img src={Logo} alt="Logo Image" style={{ width: "200px" }} />
       </div>
 
