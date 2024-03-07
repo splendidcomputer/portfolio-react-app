0c3c5d82fe205cc95ff37827211d05f40d015534
splendidcomputer <splendidcomputer@gmail.com>
2024-03-07 23:51:23 +0100

    Added mealz.

--- src/componets/Work.jsx
+++ src/componets/Work.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import WorkImg from "../assets/projects/ET.png";
+import mealz from "../assets/projects/mealz.png";
 import realEstate from "../assets/projects/realestate.jpg";
 
 const Work = () => {
@@ -27,6 +28,34 @@ const Work = () => {
               </span>
               <div className="pt-8 text-center">
                 <a href="https://emotional-territories-web.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                {/* <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+            {/* Container */}
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${mealz})` }}
+            className="shadow-md shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                A React AI Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://www.mealzz.de/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
